c4a49b9f64059758f8252889a7ec8807e74aba34
Quentin de Grandmaison <quentin.degrandmaison@dashlane.com>
2020-04-15 15:23:39 +0200

    feat: update docz to 2.3

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
 const TsConfigPathsPlugin = require('awesome-typescript-loader')
   .TsConfigPathsPlugin;
 
-module.exports.onCreateWebpackConfig = ({ actions }) => {
+module.exports.onCreateWebpackConfig = ({ actions, loaders, getConfig }) => {
   actions.setWebpackConfig({
     resolve: {
       plugins: [new TsConfigPathsPlugin()]
