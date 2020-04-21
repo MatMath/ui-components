239b43a44cc5a33c0ec277f1a268a0b80e3d68aa
Olivier Cléro <olivier.clero@dashlane.com>
2020-04-21 12:28:33 +0200

    chore: add tests for getClassNames fonction

---
+++ src/utility/cssUtils.spec.ts
@@ -0,0 +1,76 @@
+import { getClassNames } from '@utility/cssUtils';
+
+describe('getClassNames', () => {
+  describe('String parameters (no object)', () => {
+    it('should have the class name', () => {
+      const className = 'class-name';
+      const result = getClassNames(className);
+
+      const expectedResult = className;
+      expect(result).toEqual(expectedResult);
+    });
+
+    it('should have all class names separated by space', () => {
+      const className1 = 'class-name-1';
+      const className2 = 'class-name-2';
+      const className3 = 'class-name-3';
+      const result = getClassNames(className1, className2, className3);
+
+      const expectedResult = `${className1} ${className2} ${className3}`;
+      expect(result).toEqual(expectedResult);
+    });
+  });
+
+  describe('Object parameters', () => {
+    it('should have the class name', () => {
+      const className = 'class-name';
+      const result = getClassNames({
+        [className]: true
+      });
+
+      const expectedResult = className;
+      expect(result).toEqual(expectedResult);
+    });
+
+    it('should have all class names separated by space', () => {
+      const className1 = 'class-name-1';
+      const className2 = 'class-name-2';
+      const className3 = 'class-name-3';
+      const result = getClassNames({
+        [className1]: true,
+        [className2]: true,
+        [className3]: true
+      });
+
+      const expectedResult = `${className1} ${className2} ${className3}`;
+      expect(result).toEqual(expectedResult);
+    });
+
+    it('should not have class name whose value is false', () => {
+      const className1 = 'class-name-1';
+      const className2 = 'class-name-2';
+      const className3 = 'class-name-3';
+      const result = getClassNames({
+        [className1]: true,
+        [className2]: false,
+        [className3]: true
+      });
+
+      const expectedResult = `${className1} ${className3}`;
+      expect(result).toEqual(expectedResult);
+    });
+  });
+
+  describe('Mix of Object and String parameters', () => {
+    it('should have all class names separated by space', () => {
+      const className1 = 'class-name-1';
+      const className2 = 'class-name-2';
+      const result = getClassNames(className1, {
+        [className2]: true
+      });
+
+      const expectedResult = `${className1} ${className2}`;
+      expect(result).toEqual(expectedResult);
+    });
+  });
+});
