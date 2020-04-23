01dc685830bddfa64dd99cf81bfa8329ea6c9160
Arthur Coruble <arthur.coruble@dashlane.com>
2020-04-23 11:57:36 +0200

    fix(text-input): edit field to take fluid prop
    As we sometime need longer fields (ones that take all the container
    width), I implemented the `fluid` prop that makes the field fit to
    100% of it's container.

--- src/components/text-input/TextInput.tsx
+++ src/components/text-input/TextInput.tsx
@@ -7,23 +7,39 @@ type _ForbiddenProps = 'size' | 'prefix' | 'type';
 
 interface TextInputProps
   extends Omit<React.InputHTMLAttributes<HTMLInputElement>, _ForbiddenProps> {
+  /**
+   * The feedback nature of the input
+   */
   feedbackType?: TextInputFeedbackType;
+  /**
+   * Some text to display under the input when triggering a feedback type
+   */
   feedbackText?: string;
+  /**
+   * Make this field take 100% of the width of it's container
+   * @default false
+   */
+  fluid?: boolean;
 }
 
 export const TextInput: React.FC<TextInputProps> = ({
   feedbackType,
   feedbackText,
+  fluid = false,
   ...htmlInputProps
 }) => {
   const feedbackCssClass =
     feedbackType && styles[feedbackType] ? styles[feedbackType] : '';
 
+  const fluidCssClass = fluid && styles.fluid ? styles.fluid : '';
+
   return (
-    <div className={getClassNames(styles.root, feedbackCssClass)}>
+    <div
+      className={getClassNames(styles.root, feedbackCssClass, fluidCssClass)}
+    >
       <input
         {...htmlInputProps}
-        className={getClassNames(styles.input, feedbackCssClass)}
+        className={getClassNames(styles.input, feedbackCssClass, fluidCssClass)}
         type='text'
       />
       {feedbackText && feedbackType ? (
