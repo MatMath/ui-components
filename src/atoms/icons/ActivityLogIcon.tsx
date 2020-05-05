c0b2de71150c8d429a508068f5bc232722248efd
Alexandre Hervé <alexandre.herve9@gmail.com>
2020-05-05 13:08:48 +0200

    feat(icons): add new icons set
    - ActivityLogIcon
    - DashboardIcon
    - SharingGroupIcon
    - UserCircleIcon

---
+++ src/atoms/icons/ActivityLogIcon.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { SvgWrapper } from '@ions/icon/wrapper/SvgWrapper';
+import { IconProps } from '@ions/icon/types';
+
+export const ActivityLogIcon: React.FC<IconProps> = (props: IconProps) => (
+  <SvgWrapper {...props}>
+    <React.Fragment>
+      <path d='M4 1.50016H2.5V18.5002H14.2L13.3 17.0002H4V1.50016Z' />
+      <path d='M16 17.0002L16 18.5002H17.5V17.0002H16Z' />
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M16 6.31083L16 18.5002H17.5L17.5 5.68951L13.3107 1.50016L2.5 1.50016L2.5 3.00016L12.6893 3.00016L16 6.31083Z'
+      />
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M11.8093 1.5H13.3093L13.3093 5.68328L17.5 5.68342L17.5 7.18342L11.8093 7.18323L11.8093 1.5Z'
+      />
+      <path d='M8.5 10.5L13.6016 10.4997L14.5 9.00097L8.5 9V10.5Z' />
+      <path d='M13.5996 13.7477L8.5 13.75V12.25H14.4975L13.5996 13.7477Z' />
+      <path d='M5.5 9H7V10.5H5.5V9Z' fill='#0E353D' />
+      <path d='M7 12.25H5.5V13.75H7V12.25Z' fill='#0E353D' />
+    </React.Fragment>
+  </SvgWrapper>
+);
