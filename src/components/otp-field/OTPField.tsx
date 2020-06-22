b0e8961af0f043f8bd86aaef134f9adcde00b43d
Francois Hendriks <francoishendriks96@gmail.com>
2020-06-22 10:44:04 +0200

    chore(otp-field): add otp shuffler

--- src/components/otp-field/OTPField.tsx
+++ src/components/otp-field/OTPField.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import { TimerClock, TimerClockProps } from '@components/otp-field/TimerClock';
 import { formatCode } from '@components/otp-field/helpers';
+import { useOTPCodeShuffler } from '@components/otp-field/otp-code-shuffler/useCodeShuffler';
 import styles from './OTPField.module.scss';
 
 export interface OTPFieldProps extends TimerClockProps {
@@ -9,23 +10,40 @@ export interface OTPFieldProps extends TimerClockProps {
   /**
    * A 6-character otp code string
    * No space character should be within the code
+   * If null is provided, the component will shuffle the code
    */
-  code: string;
+  code: string | null;
 }
 
 export const OTPField = ({
   code,
   label,
+  onTimerEnd,
   ...clockProps
 }: OTPFieldProps): JSX.Element => {
-  const formattedCode = formatCode(code);
+  const [isShuffling, setIsShuffling] = React.useState(false);
+
+  const resetCode = React.useCallback(() => {
+    setIsShuffling(true);
+    onTimerEnd && onTimerEnd();
+  }, [onTimerEnd]);
+
+  React.useEffect(() => {
+    setIsShuffling(!code);
+  }, [code]);
+
+  const { shuffledCode } = useOTPCodeShuffler({
+    shouldShuffle: isShuffling
+  });
+
+  const formattedCode = formatCode(isShuffling || !code ? shuffledCode : code);
 
   return (
     <div className={styles.root}>
       <span className={styles.label}>{label}</span>
       <div className={styles.container}>
         <div className={styles.code}>{formattedCode}</div>
-        <TimerClock {...clockProps} />
+        <TimerClock onTimerEnd={resetCode} {...clockProps} />
       </div>
     </div>
   );
