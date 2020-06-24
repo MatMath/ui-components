d42871a95e6475671cbe8f8a4c51989e6724befc
pducolin <paola.ducolin@dashlane.com>
2020-06-24 18:10:48 +0200

    chore: import jest-dom at jest setup

--- src/components/slider/Slider.spec.tsx
+++ src/components/slider/Slider.spec.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { fireEvent, render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
 
 import { Slider } from './Slider';
 
