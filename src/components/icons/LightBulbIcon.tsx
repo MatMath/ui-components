61375582ed1d1f98e013e6e15b0d35e0242d7afa
Quentin L'Hours <quentin.lhours@dashlane.com>
2020-07-02 13:43:09 +0200

    feat(icon): add light bulb icon

---
+++ src/components/icons/LightBulbIcon.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { IconProps } from '@components/icons/types';
+import { SvgWrapper } from '@components/icons/utils/SvgWrapper';
+
+/*
+ * /!\ This is a generated file, do not update it or create a similar one by hand.
+ * Please refer to the doc in `documentation/ICON_STEPBYSTEP.md` to learn how to deal with icons.
+ */
+
+export const LightBulbIcon: React.FC<IconProps> = (props: IconProps) => (
+  <SvgWrapper {...props}>
+    <React.Fragment>
+      <path d='M8.5 20H13.5L12.5 22H9.5L8.5 20Z' />
+      <path d='M19 9H21.5V11H19V9Z' />
+      <path d='M12 8.74224e-08V2.5H10V0L12 8.74224e-08Z' />
+      <path d='M2.39424 3.9813L4.16201 5.74907L5.57622 4.33486L3.80845 2.56709L2.39424 3.9813Z' />
+      <path d='M19.2679 3.99922L17.5001 5.76699L16.0859 4.35277L17.8537 2.58501L19.2679 3.99922Z' />
+      <path d='M0.5 9H3V11H0.5V9Z' />
+      <path
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M13.5 16.456C15.5655 15.5079 17 13.4215 17 11C17 7.68629 14.3137 5 11 5C7.68629 5 5 7.68629 5 11C5 13.4215 6.43447 15.5079 8.5 16.456C8.55441 16.4809 8.60926 16.5051 8.66453 16.5285C9.38236 16.8321 10.1716 17 11 17C11.8922 17 12.7389 16.8053 13.5 16.456ZM11 15C13.2091 15 15 13.2091 15 11C15 8.79086 13.2091 7 11 7C8.79086 7 7 8.79086 7 11C7 13.2091 8.79086 15 11 15Z'
+      />
+      <path d='M13.5 16.456C12.7389 16.8053 11.8922 17 11 17C10.1716 17 9.38236 16.8321 8.66453 16.5285C8.60926 16.5051 8.55441 16.4809 8.5 16.456V18H13.5V16.456Z' />
+    </React.Fragment>
+  </SvgWrapper>
+);
+
+/*
+ * /!\ This is a generated file, do not update it or create a similar one by hand.
+ * Please refer to the doc in `documentation/ICON_STEPBYSTEP.md` to learn how to deal with icons.
+ */
